675440df19ec9933ca3aff165ee2b17ced2de126
Facundo Gil <facundo_gil@hotmail.com.ar>
2023-08-31 00:07:52 -0300

    integracion de funciones

--- main.js
+++ main.js
@@ -5,13 +5,29 @@ function bienvenida() {
     `Hola ${nombre}, esta aplicacion te va a ayudar a sumar el precio de tus productos y calcular tus cuotas.`
   );
 }
-function cantidadProductos() {
-  bienvenida();
-  let cantidades = parseFloat(prompt("¿Cuántos productos deseas calcular?"));
+function PrecioProductos() {
+  
+  let precios = parseFloat(prompt("Ingresa el precio de tus productos"));
 
-  while (isNaN(cantidades)) {
-    cantidades = parseFloat(prompt("Ingrese un numero valido"));
+  while (isNaN(precios)) {
+    precios = parseFloat(prompt("Ingrese un numero valido"));
   }
-  return cantidades;
+  return precios;
 }
-cantidadProductos();
+
+
+function sumaPrecios() {
+    bienvenida();
+
+    let cantidad = parseInt(prompt("¿Cuantos Productos deseas agregar?"));
+    let suma = 0;
+    
+    for (let i = 0; i < cantidad; i++) {
+        const numero = PrecioProductos();
+        suma += numero;
+        
+    }
+    return suma;
+}
+const resultado = sumaPrecios();
+alert(`La suma total es: ${resultado}`);
